5bd3622ffe5d4acaf72f7091b998056f4624a208
Melania Ion <melaniaion24@gmail.com>
2024-04-07 12:42:57 +0300

    Update Category Repository

--- InventoryManagerProject/InventoryManagerDataAccess/Interfaces/ICategoryRepository.cs
+++ InventoryManagerProject/InventoryManagerDataAccess/Interfaces/ICategoryRepository.cs
@@ -7,7 +7,7 @@ namespace InventoryManagerDataAccess.Interfaces
         public int Create(Category category);
         public List<Category> GetAll();
         public Category Get(int id);
-        public void Update(int id, Category updatedCategory, Category categoryToUpdate);
-        public void Delete(int id, Category category);
+        public void Update(Category updatedCategory, Category categoryToUpdate);
+        public void Delete(Category category);
     }
 }
---
+++ InventoryManagerProject/InventoryManagerDataAccess/Interfaces/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using InventoryManagerDataAccess.Entities;
+
+namespace InventoryManagerDataAccess.Interfaces
+{
+    public interface IProductRepository
+    {
+        public int Create(Product product);
+        public List<Product> GetAll();
+        public Product Get(int id);
+        public void Update(Product updatedProduct, Product productToUpdate);
+        public void Delete(Product product);
+    }
+}
