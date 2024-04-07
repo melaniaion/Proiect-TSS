5befac7f0271fa1bd5584ebbd7b84ed4a042dafd
Melania Ion <melaniaion24@gmail.com>
2024-04-07 13:41:26 +0300

    Add interface IProductService

---
+++ InventoryManagerProject/InventoryManagerBusiness/Interfaces/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using InventoryManagerBusiness.DTOs;
+
+namespace InventoryManagerBusiness.Interfaces
+{
+    public interface IProductService
+    {
+        int Create(ProductRequest productDto);
+        List<ProductResponse> Get();
+        ProductResponse Get(int id);
+        void Update(int id, ProductRequest updatedProductDto);
+        void Delete(int id);
+    }
+}
