263010c8db66ae3c8c4528c12a420c8eb1b028cd
Melania Ion <melaniaion24@gmail.com>
2024-04-07 13:15:32 +0300

    Add ProductResponse DTO

---
+++ InventoryManagerProject/InventoryManagerBusiness/DTOs/ProductResponse.cs
@@ -0,0 +1,15 @@
+﻿
+namespace InventoryManagerBusiness.DTOs
+{
+    public class ProductResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Stock { get; set; }
+        public double DiscountedPrice => FullPrice - (FullPrice * Discount / 100);
+        public int Discount { get; set; }
+        public double FullPrice { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
